e9505372b0edf06716ee72d4036d7ea8e2538f7b
Scott Windsor <windsorwindsor2@protonmail.com>
2025-03-04 21:02:01 -0500

    Added get_weight

--- haywire/haywire.py
+++ haywire/haywire.py
@@ -8,16 +8,18 @@ class Mat:
         self.main_spc = main_spc
         self.cross_spc = cross_spc
 
+        #assume that wire area is the size/100.
+        #May change this later to support metric.
         self.main_type,self.main_size = self._get_wire_size(main_size)
         self.cross_type,self.cross_size = self._get_wire_size(cross_size)
 
         #wire_parameters
         self.min_clr_from_edge = 0.5
+        self.unit_wt=0.2836 #lbs/in**3
 
         #calculated values
         self.nmain = self._calc_wires(self.width, self.main_spc)
         self.ncross = self._calc_wires(self.length, self.cross_spc)
-        
 
     def __str__(self):
         mat_desc=f'{self.width}"x{self.length}" {self.main_spc}"x{self.cross_spc}" D{self.main_size}/{self.cross_size}'
@@ -39,8 +41,9 @@ class Mat:
 
     def _calc_wires(self,length,spc):
         return math.floor((length - 2 * self.min_clr_from_edge)/spc)
-        
     
-    
-        
-            
+    def get_weight(self):
+        #assumes US units, weight in lbs
+        main_vol=self.length*(self.main_size/100)*self.nmain
+        cross_vol=self.width*(self.cross_size/100)*self.ncross
+        return (main_vol+cross_vol)*self.unit_wt
